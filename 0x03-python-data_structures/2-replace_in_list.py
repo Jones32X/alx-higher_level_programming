1c7f47020430a339cdcc3418b03e3fea584889e0
Jones32X <jonespedro.Nezingu@gmail.com>
2024-03-18 09:13:05 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     listLen = len(my_list) - 1
     if idx < 0 or idx > listLen:
         return (myList)
