8c1b5228a0f3afb004604d5ffdf4991f44215dad
Jones32X <jonespedro.Nezingu@gmail.com>
2024-03-20 04:58:12 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import sys
 if __name__ == '__main__':
-    import sys
     sys.argv.pop(0)
     tot = 0
     for num in sys.argv:
-        tot += int(num)
-        print("{}".format(tot))
+        tot = tot + int(num)
+    print("{}".format(tot))
