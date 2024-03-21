29d9453c6284fae6aa535cb0e5436d6a0e9a293e
Jones32X <jonespedro.Nezingu@gmail.com>
2024-03-21 15:16:32 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-def multiply_by_2(a_dictionary):
-    N_Dicti = a_dictionary.copy()
-    for key in N_Dicti:
-         N_Dicti[key] *= 2
-    return(N_Dicti)
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary.keys():
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return(a_dictionary)
