2512f80226f7f97207cedb3bd14d919e0f4e7757
Jones32X <jonespedro.Nezingu@gmail.com>
2024-04-17 15:54:22 -0700

    4-print_square.py, tests/4-print_square.txt

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,7 +16,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        for j in range(size):
+    for X in range(size):
+        for P in range(size):
             print('#', end='')
         print()
